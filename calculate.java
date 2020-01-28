3356b4495f1ee5ff63dfccfba1eecf0b3f9ef620
siarheinovikau <50303110+siarheinovikau@users.noreply.github.com>
2020-01-29 01:23:37 +0300

    creat calculate

--- calculate.java
+++ calculate.java
@@ -1,5 +1,9 @@
 public class calculate{
 	public static void main(String[] arg){
 		System.out.println("calculate . . .");
+			int first = Integer.valueOf(arg[0]);
+			int second = Integer.valueOf(arg[1]);
+			int summ = first + second;
+			System.out.println("Sum " +  summ);
 	}
 }
